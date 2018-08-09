9fde97578d42b98ca0827a08494c837c3a15af5e
Usuário de Teste <techne@techne.com.br>
2018-08-09 15:51:08 -0300

    ajustes...

--- src/main/java/SpringBootMain.java
+++ src/main/java/SpringBootMain.java
@@ -1,12 +1,12 @@
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
